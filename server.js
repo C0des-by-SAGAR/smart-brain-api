b3f7ff91b90b90a740c9fd7d05dac6e93917af58
Sagar Singh <codersagar007@gmail.com>
2025-08-16 15:59:22 +0530

    changes made

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
@@ -21,38 +22,17 @@ const db = knex({
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
 
-app.get('/', (req, res) => {
-	res.send(database.users);
-})
+app.use(cors())
+app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
-app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+app.get('/', (req, res)=> { res.send(db.users) })
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
-
-app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
-
-app.put('/image', (req, res) => {image.handleImage(req, res, db)})
-
-app.post('/imageurl', (req, res) => {
-  const CLARIFAI_URL = "https://api.clarifai.com/v2/models/face-detection/outputs";
-  const raw = JSON.stringify(req.body);
-
-  fetch(CLARIFAI_URL, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Authorization': 'Key 0c110e74cc6540099dce33e4177a9717'
-    },
-    body: raw
-  })
-    .then(apiRes => apiRes.json())
-    .then(data => res.json(data))
-    .catch(err => res.status(400).json('Unable to work with API'));
-});
-
-app.listen(3000, () => {
-	console.log('app is running on port 3000');
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
 })
