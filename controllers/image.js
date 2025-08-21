b3ded6a090dac054603b9e6b9c5b4f35c359bee7
Sagar Singh <codersagar007@gmail.com>
2025-08-22 04:46:17 +0530

    image.js fix

--- controllers/image.js
+++ controllers/image.js
@@ -1,32 +1,57 @@
-const Clarifai = require('clarifai');
-
-const app = new Clarifai.App({
- apiKey: process.env.CLARIFAI_API_KEY
-});
+const fetch = require('node-fetch');
 
 const handleApiCall = (req, res) => {
-  app.models.predict('a403429f2ddf4b49b307e318f00e528b', req.body.input)
+  const raw = JSON.stringify({
+    "user_app_id": {
+      "user_id": "clarifai",   // default public user
+      "app_id": "main"         // default app
+    },
+    "inputs": [
+      {
+        "data": {
+          "image": {
+            "url": req.body.input
+          }
+        }
+      }
+    ]
+  });
+
+  fetch("https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Authorization": "Key " + process.env.CLARIFAI_API_KEY,
+      "Content-Type": "application/json"
+    },
+    body: raw
+  })
+    .then(response => response.json())
     .then(data => {
+      if (data.status.code !== 10000) {
+        console.error("Clarifai API error:", data);
+        return res.status(400).json("Unable to work with API");
+      }
       res.json(data);
     })
     .catch(err => {
       console.error("Clarifai API error:", err);
-      res.status(400).json('Unable to work with API');
-    })    
-}
+      res.status(400).json("Unable to work with API");
+    });
+};
 
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0].entries);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
-}
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'));
+};
 
 module.exports = {
   handleImage,
   handleApiCall
-}
+};
